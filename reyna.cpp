71ea3557080ac6074a6aedaf42d58f756509d0b1
Jitiwat2513 <139094628+Jitiwat2513@users.noreply.github.com>
2023-07-17 10:15:27 +0700

    Add files via upload

---
+++ reyna.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string Name;
+	int Year;
+	cout <<"Enter Name          : ";
+	cin >> Name;
+	cout <<"Enter Year of birth : ";
+	cin >> Year;
+	cout <<"\nYour Age = "<<Year-2526<<" years old"<<endl;
+	
+	system("pause");
+	return(0);
+}
+
